9ef2d287cb3aae4ca767d979645894e51df9766b
Priscilla Then <priscillathen@gmail.com>
2015-09-11 12:43:33 -0700

    FAFIA - Change text
    Tweak existing text, add descriptions for logo and timeline sections.

--- portfolio-items/fafia.php
+++ portfolio-items/fafia.php
@@ -49,7 +49,8 @@ Author: Priscilla Then, Feb. 23, 2015
                     <h3>New Website</h3>
                     <p>The new website only contains essential information. The most important goal
                         was to get donations, so the "Donate" button is repeated in bright yellow at the top and bottom of
-                        every page. The secondary goal was to educate and serve as a resource to academic scholars and to educate the public.</p>
+                        every page. The secondary goal was to educate and serve as a resource to academic scholars and to educate the public. To view the new website, go to: <a href="http://www.fafia-afai.org/" target="_blank">www.fafia-afai.org</a>.
+                    </p>
                 </li>
                 <li>
                     <h3>TECHNICAL</h3>
@@ -57,12 +58,17 @@ Author: Priscilla Then, Feb. 23, 2015
                         (English & French), using the Wordpress Multilingual extension.</p>
                     <p>The design was created as a 2D mockup in Photoshop, then brought to life using Bootstrap 3.</p>
                 </li>
+                <li>
+                    <h3>LOGO</h3>
+                    <p>The original logo was uninspired, representing women with a washroom symbol through a clichéd pink colour. I strongly suggested that FAFIA revamp their logo, and they were glad that they did. FAFIA wanted their new logo to be strong yet sophisticated, so I created a logo with several hands supporting and uplifting one another, to symbolize the collective strength of community when they work in unison. The colours are representative of a flame, symbolizing energy. A good logo is unique and memorable, and I think this logo does just that.
+                    </p>
+                </li>
+                <li>
+                    <h3>TIMELINE</h3>
+                    <p>Since FAFIA's website is educational, they wanted to show the evolution of relevant issues and events through a timeline. The text was long and dry to read, so I broke it up through coloured sections, alternating right and left sides for visual interest, and to make a text-heavy section more enjoyable to read.</p>
+                    <p>The elements used on the website use the same colour palette as in the logo, for unity in design and reinforcement of their branding colours.</p>
+                </li>
             </ul>
-
-
-
-
-
 
         </div>
     </div>
