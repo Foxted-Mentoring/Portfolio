798365b152c801aaa8d2f45db7257d668f01456c
Priscilla Then <priscillathen@gmail.com>
2015-09-11 11:15:43 -0700

    FAFIA: Update text

--- portfolio-items/fafia.php
+++ portfolio-items/fafia.php
@@ -35,25 +35,27 @@ Author: Priscilla Then, Feb. 23, 2015
             <ul>
                 <li>
                     <h3>Audience</h3>
-                    <p>The United Nations, university scholars, and the general public.</p></li>
+                    <p>The United Nations, the Government of Canada, university scholars,and the general public.</p></li>
                 <li>
                     <h3>Mandate</h3>
-                    <p>To provide academic resources to hold the government accountable for their promises to improve the situation for women in Canada.</p>
+                    <p>To provide academic resources to hold the Canadian government accountable for their promises to improve the status of women in Canada.</p>
                 </li>
                 <li>
                     <h3>Old Website</h3>
-                    <p>The original website was cluttered with information and was not logically grouped for information to be easily found.</p>
-                    <p>Not responsive for mobile devices. Click here to <a href="http://jolly-joey-05.gopagoda.com">view the
-                            old website</a>.</p>
+                    <p>The original website was cluttered with information and information was not logically grouped to make things easy to find.</p>
+                    <p>The website was also not responsive for mobile devices.</p>
                 </li>
                 <li>
                     <h3>New Website</h3>
-                    <p>The new website strips away all unnecessary elements, leaving the essentials. The most important goal
+                    <p>The new website only contains essential information. The most important goal
                         was to get donations, so the "Donate" button is repeated in bright yellow at the top and bottom of
-                        every page.</p>
+                        every page. The secondary goal was to educate and serve as a resource to academic scholars and to educate the public.</p>
+                </li>
+                <li>
+                    <h3>TECHNICAL</h3>
                     <p>The new website is a custom-made Wordpress website that is responsive as well as multi-lingual
-                        (English & French).</p>
-                    <p>The design was built from scratch with 2D mockups in Photoshop, then brought to life using Bootstrap.</p>
+                        (English & French), using the Wordpress Multilingual extension.</p>
+                    <p>The design was created as a 2D mockup in Photoshop, then brought to life using Bootstrap 3.</p>
                 </li>
             </ul>
 
