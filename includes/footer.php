ac806f334a398c2ff45113eddba13e2b274fc3e2
Priscilla Then <priscillathen@gmail.com>
2015-09-11 19:52:32 -0700

    Minor tweaks

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 
         <ul class="right">
             <li><a href="http://linkedin.com/in/priscillathen/en" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="http://twitter.com/priscillabiju" target="_blank"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="http://twitter.com/priscillathen" target="_blank"><i class="fa fa-twitter"></i></a></li>
             <li><a href="http://www.facebook.com/priscilla.biju" target="_blank"><i class="fa fa-facebook"></i></a></li>
         </ul>
     </div>
