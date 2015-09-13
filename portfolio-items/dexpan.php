0b3fb7b6a670d30af9820fc30c270dff9e79a0e3
Priscilla Then <priscillathen@gmail.com>
2015-09-13 13:06:13 -0700

    Add descriptive text - Dexpan

--- portfolio-items/dexpan.php
+++ portfolio-items/dexpan.php
@@ -1,33 +1,35 @@
-<h1>Dexpan</h1>
-<h2>Branding, e-commerce</h2>
+<h1>Dexpan Canada</h1>
+<h2>Branding, e-commerce Website</h2>
 
 <div class="item-images">
-    <img src="images/portfolio-item-images/dexpan.png" alt="Dexpan homepage">
+    <img src="images/portfolio-item-images/dexpan-before-after-med.png" alt="Dexpan homepage">
+    <img src="images/portfolio-item-images/dexpan-products.png" alt="Dexpan homepage">
+    <img src="images/portfolio-item-images/dexpan-services.png" alt="Dexpan homepage">
 </div>
 
 <div class="item-description">
     <ul>
         <li>
             <h3>Audience</h3>
-            <p>The United Nations, university scholars, and the general public.</p></li>
+            <p>Building contractors and home owners wanting to break up and remove rock or concrete.</p></li>
         <li>
             <h3>Mandate</h3>
-            <p>To provide academic resources to hold the government accountable for their promises to improve the situation for women in Canada.</p>
+            <p>To redesign the company logo and replace their old static website with a dynamic, e-commerce website.</p>
         </li>
         <li>
             <h3>Old Website</h3>
-            <p>The original website was cluttered with information and was not logically grouped for information to be easily found.</p>
-            <p>Not responsive for mobile devices. Click here to <a href="http://jolly-joey-05.gopagoda.com">view the
-                    old website</a>.</p>
+            <p>The original website was cluttered, and information was not logically grouped and in some cases, redundant. No picture of the actual product was shown, nor was it clearly explained. The header was very tall and did not maximize the precious space  "above-the-fold".</p>
         </li>
         <li>
             <h3>New Website</h3>
-            <p>The new website strips away all unnecessary elements, leaving the essentials. The most important goal
-                was to get donations, so the "Donate" button is repeated in bright yellow at the top and bottom of
-                every page.</p>
-            <p>The new website is a custom-made Wordpress website that is responsive as well as multi-lingual
-                (English & French).</p>
-            <p>The design was built from scratch with 2D mockups in Photoshop, then brought to life using Bootstrap.</p>
+            <h4>Information Architecture</h4>
+            <p>The new website required a complete reorganization of information. The navigation was split into a primary and secondary menu, depending on importance and relevance of the info ( i) Main Navigation: Products, Services, Resources, etc.; ii) Secondary Navigation: About Us, Contact Us, Join Us, etc.)</p>
+            <h4>Copywriting</h4>
+            <p>Since the target was homeowners and building contractors, a casual yet professional tone was used. Since the sequence of information to be conveyed works in tandem with the layout and imagery, the key concepts and strategies are revealed below.
+            <h4>Home Page</h4>
+            <p>The home page shows off the major points to be conveyed to customers. Starting off with a banner showing the end result for one of their B2C clients (a homeowner's garden), followed by shots of the powder product and how it mixes with water to give a quick understanding of how the product works. Then a section on how to prepare the product follows, the different grades for different temperature ranges, a section linking to their products page, and the most recent use cases are shown as a blog. All these sections work in tandem to give a snapshot of what the product is, how it can be used, its benefits and features, as well as blog stories with real customers as hard proof that their product works.</p>
+            <h4>Products Page</h4>
+            <p>Both customers and warehouse staff often mixed up the different types of powders, ordering or shipping the wrong order. The owner asked that labels be created to distinguish the powdered mixes from one another, and were not only placed on the shipping boxes, but were also used on the website for consistency. The products are also listed in terms of quantity to offer buyers a visual depiction of their order.</p>
         </li>
     </ul>
 </div>
