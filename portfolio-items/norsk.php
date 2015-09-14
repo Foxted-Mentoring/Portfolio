582c1b8ab4982b476bacb9b4ea4917b648d5c587
Priscilla Then <priscillathen@gmail.com>
2015-09-13 19:36:08 -0700

    Norse - replace text

--- portfolio-items/norsk.php
+++ portfolio-items/norsk.php
@@ -1,33 +1,51 @@
-<h1>Norsk</h1>
+<h1>Norsk Interior Design</h1>
 <h2>Branding, Website redesign</h2>
 
 <div class="item-images">
-    <img src="images/portfolio-item-images/norsk.png" alt="Norsk homepage">
+    <img src="images/portfolio-item-images/norsk-homepage-before-after.png" alt="homepage before & after">
+    <img src="images/portfolio-item-images/norsk-about-before-after.png" alt="about before & after">
+    <img src="images/portfolio-item-images/norsk-faq-before-after.png" alt="FAQ before & after">
+    <img src="images/portfolio-item-images/norsk-facebook.png" alt="facebook banner">
 </div>
 
 <div class="item-description">
     <ul>
         <li>
             <h3>Audience</h3>
-            <p>The United Nations, university scholars, and the general public.</p></li>
+            <p>Home owners in the Greater Vancouver area who are looking for home staging to resell a house or to renovate and/or redecorate their home.</p></li>
         <li>
             <h3>Mandate</h3>
-            <p>To provide academic resources to hold the government accountable for their promises to improve the situation for women in Canada.</p>
+            <p>To help with rebranding and redesigning the client's existing website.</p>
+        </li>
+        <li>
+            <h3>Branding</h3>
+            <h4>Company Name</h4>
+            <p>Doing a little research revealed that the name "Sense of Style" was already taken by three other designer in different parts of the United States. I therefore suggested that we do a complete revamp to her branding, starting from the bottom up, to avoid conflicting names and confusion, as well as giving her a stronger identity.</p>
+            <h4>Logo</h4>
+            <p>An apple blossom, characteristic of the apple orchards in Southern Norway, was used to as the client's logo. The traditional colours of deep red, navy, smoke blue and white were used to convey crisp, clean, nautical colours. As the client was an interior designer, coming up with a suitable colour palette was an enjoyable experience, as she knew exactly which colours she wanted. I drew the apple blossom by hand, then redrew it in Illustrator and coloured it with the client's chosen colours.</p>
+            <h4>Tagline</h4>
+            <p>Since what makes the client unique is her Scandinavian heritage, we opted for "Scandinavian Inspiration" as her slogan.</p>
         </li>
         <li>
             <h3>Old Website</h3>
-            <p>The original website was cluttered with information and was not logically grouped for information to be easily found.</p>
-            <p>Not responsive for mobile devices. Click here to <a href="http://jolly-joey-05.gopagoda.com">view the
-                    old website</a>.</p>
+            <p>The original website's look was outdated, dark and sombre, and was in much need of a facelift. The copy was generic, with no mention of what made the designer unique; nor was it tailored to her services. The website also lacked any social media links. From a technical standpoint, the website was static, so she could not modify it herself; nor was it responsive.</p>
         </li>
         <li>
             <h3>New Website</h3>
-            <p>The new website strips away all unnecessary elements, leaving the essentials. The most important goal
-                was to get donations, so the "Donate" button is repeated in bright yellow at the top and bottom of
-                every page.</p>
-            <p>The new website is a custom-made Wordpress website that is responsive as well as multi-lingual
-                (English & French).</p>
-            <p>The design was built from scratch with 2D mockups in Photoshop, then brought to life using Bootstrap.</p>
+            <h4>Copywriting</h4>
+            <p>The client's number of years of industry experience and her Scandinavian-influenced designs were what made her unique, and were stated up front and center.</p>
+            <h4>Platform</h4>
+            <p>The client needed a website that she could update herself, so Wordpress was the CMS of choice for its relative simplicity.</p>
+            <h4>Home Page</h4>
+            <p>The same images were used, but made larger for more visual appeal, with splashes of vibrant coral to add some vitality.</p>
+            <h4>About</h4>
+            <p>A more recent, professional and flattering photo was used, and the copy was geared to only the most relevant points that would interest her target audience.</p>
+            <h4>FAQ</h4>
+            <p>The logo was used in a drop-down accordion to condense a text-heavy page, while reinforcing the branding.</p>
+        </li>
+        <li>
+            <h3>Facebook Banner</h3>
+            <p>The logo was reused in the Facebook banner, listing the client's principal services for promotion and reinforcement.</p>
         </li>
     </ul>
 </div>
