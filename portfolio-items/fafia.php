6de92d0eec6c5c71839a9d9ee8cfcf2e9ac14dac
Priscilla Then <priscillathen@gmail.com>
2015-09-15 14:43:03 -0700

    FAFIA: replace images with clear background

--- portfolio-items/fafia.php
+++ portfolio-items/fafia.php
@@ -2,9 +2,12 @@
 <h2>Branding + Website Redesign</h2>
 
 <div class="item-images">
-    <img src="images/portfolio-item-images/fafia-homepage.jpg" alt="FAFIA homepage comparison">
-    <img src="images/portfolio-item-images/fafia-logos-comparison.gif" alt="logo comparison old vs. new">
-    <img src="images/portfolio-item-images/fafia-timeline.jpg" alt="timeline">
+    <img src="images/portfolio-item-images/fafia-homepage-before-after.jpg" alt="FAFIA homepage comparison">
+    <img src="images/portfolio-item-images/fafia-logo-before-after.png" alt="logo comparison old vs. new">
+    <img src="images/portfolio-item-images/fafia-colours-symbolism.gif" alt="logo comparison old vs. new">
+    <img src="images/portfolio-item-images/fafia-colours-hierarchy.gif" alt="logo comparison old vs. new">
+    <img src="images/portfolio-item-images/fafia-colours-breakdown.gif" alt="logo comparison old vs. new">
+    <img src="images/portfolio-item-images/fafia-timeline.png" alt="timeline">
 </div>
 
 <div class="item-description">
