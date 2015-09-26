9e850e67dda0ec7aae365f3665f8896edf70d035
Priscilla Then <priscillathen@gmail.com>
2015-09-25 17:53:17 -0700

    Add NW wireframe - homepage

--- portfolio-items/nwseatcovers.php
+++ portfolio-items/nwseatcovers.php
@@ -5,6 +5,7 @@
     <img src="images/portfolio-item-images/nw-homepage-before-after-med.png" alt="NW Seat Covers homepage">
     <img src="images/portfolio-item-images/nw-homepage-mobile.png" alt="NW Seat Covers homepage">
     <img src="images/portfolio-item-images/architecture-before.jpg" alt="site-architecture-before">
+    <img src="images/portfolio-item-images/nw-wireframe-home.jpg" alt="wireframe-homepage">
     <img src="images/portfolio-item-images/nw-who-we-are.png" alt="who we are portfolio page">
 </div>
 
