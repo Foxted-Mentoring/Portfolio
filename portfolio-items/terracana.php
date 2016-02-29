d2060a4398d86d2333365beb4d83b1d8eb57ddea
Priscilla Then <priscillathen@gmail.com>
2016-02-28 16:19:42 -0800

    Add TerraCana images

---
+++ portfolio-items/terracana.php
@@ -0,0 +1,85 @@
+<!--
+ * Created by PhpStorm.
+ * User: priscilla
+ * Date: 2016-02-27
+ * Time: 7:41 PM
+ -->
+
+<h1>TerraCana Foundation Solutions Inc</h1>
+<h2>Branding, Website Redesign</h2>
+
+<div class="item-images">
+    <img src="images/portfolio-item-images/terracana-before-after.png" alt="TerraCana website before & after">
+    <img src="images/portfolio-item-images/terracana-info-arch-horizontal.png" alt="site architecture before and after">
+    <img src="images/portfolio-item-images/terracana-wireframes.jpg" alt="wireframes">
+    <img src="images/portfolio-item-images/terracana-font-tests.jpg" alt="font tests">
+    <img src="images/portfolio-item-images/terracana-palettes.jpg" alt="colour palette tests">
+    <img src="images/portfolio-item-images/terracana-style-tiles.jpg" alt="Style Tiles">
+    <img src="images/portfolio-item-images/terracana-mockup-home.png" alt="home page mockup">
+</div>
+
+<div class="item-description">
+    <ul>
+        <li>
+            <h3>Audience</h3>
+            <p>An 80/20 split between commercial and residential customers in BC, Alberta and the Yukon.</p></li>
+        <li>
+            <h3>Mandate</h3>
+            <p>I started the process by interviewing the company owners and project manager to learn what the main goals of their website. It was determined that they wanted to revamp their existing website into a brochure website that would generate leads from both organic and direct search results.
+            </p>
+        </li>
+
+        <li>
+            <h4>Understanding the User</h4>
+            <p>I placed mages of their most popular vehicles, to emphasize the customized nature of their products, and to lead into the Vehicle Selector field. Keeping in mind how readers scan rather than read text on the Web, I wanted to get rid of the text-heavy paragraphs, and replace them with lots images and descriptive text, to entice customers to actually read the text, and quickly understand the key points.</p>
+        </li>
+
+        <li>
+            <h3>Website & Competitor Audit</h3>
+            <p>Next, I performed a website audit to determine the strengths and weaknesses of their existing site. I then compared it to their top 5 competitors and analyzed their website from a design, content and navigational perspective.</p>
+            <p>It was determined that 4/5 out of their top competitors were not up to par in their website designs, giving them a clear advantage in their online presence.</p>
+        </li>
+
+        <li>
+            <h3>Information Architecture</h3>
+            <p>Next, a site map of their existing information architecture was created. After consulting with the client on what content was missing, a new site map was created &emsp; one that had more content yet that was also streamlined to reduce redundancy of information, while making key contact information present in the header and footer of every page. A site map was also added to the footer.</p>
+        </li>
+
+        <li>
+            <h3>Branding</h3>
+            <p>With clients such as BC Hydro, Vancouver International Airport, City of West Vancouver and Whistler, TerraCana was in dire need for a website that reflected the calibre of their clients.
+            </p>
+            <h4>Colour Palette Tests</h4>
+            <p>One of the constraints was that I had to work with their old logo, as they had already invested significant resources in print materials (equipment, and vehicle signage, employee uniforms, etc.). However, I found that their red and grey colours were too monotonous, so I added splashes of navy and slate blue for variety. Since their audience was a 2:3 ratio of women to men, it was important to keep the colours gender neutral, yet the colours had to be somewhat conservative to emanate an the air of credibility and trust that the company so deserved.</p>
+            <h4>Font Tests</h4>
+            <p></p>
+
+            <h4>Style Tiles</h4>
+            <p></p>
+        </li>
+        <li>
+            <h3>Old Website</h3>
+            <p>The old home page was relatively clean and modern, but far too text-heavy, almost as if it was "stuffed" with keywords. Some of its top menu bar and footer elements were confusing or redundant (nomenclature such as "My Vehicle", Payments, contact info hard to find, missing store hours, etc.).</p>
+        </li>
+
+        <li>
+            <h3>New Website</h3>
+            <h4>Requirements & Constraints</h4>
+            <p> Redesigning the new website was striking a balance between pleasing several people: 1)&nbsp;Stakeholders, who wanted customers to be able to choose their products by vehicle make/model, or by fabric type; 2) Internet marketing specialists (Graphically Speaking, a reputed Vancouver-based digital marketing agency whom they work with), who emphasized the importance of having enough text for SEO purposes; 3) the Web Developer, whose technical constraints guided my design; and finally, 4) the Visual & UX Designer (myself), where I strived for a balance between visual aesthetics and the practical aspects of user needs.</p>
+            <h4>Visual Design</h4>
+            <p>NW Seat Covers sells well-constructed, high quality products, so I wanted to reflect that in the website's look and feel through contemporary sophistication and minimalism. Since their customers are an equal gender split, I wanted to keep their look gender neutral.</p>
+            <h4>Photography</h4>
+            <p>Previously there was no visual nor verbal explanation of the categories, so I also used the company's photography studio to photograph their fabrics by category, to allow customers  to better understand the types of fabrics in each category, so that they could choose a product by fabric type. Customers could also choose to see all the fabrics by clicking the "See All Our Fabrics" button.</p>
+            <h4>Emphasizing USP</h4>
+            <p>It was also important to describe what makes NW Seat Covers unique, and why customers should choose their product over competitors, so I included a "25 years" seal to highlight their years of experience, as well as video and images to reinforce what gives the brand their competitive edge.</p>
+            <h4>Testimonials</h4>
+            <p>The number of testimonials visible on the Home Page was reduced to save precious real estate; if customers want to real more reviews, they could click on the link to "See all reviews".</p>
+            <h4>Responsive Design</h4>
+            <p>Mobile and tablet versions were made as well, keeping all the same elements, features and content as in the desktop version. The images for the web banner were customized for mobile, and stacked vertically. The clickable areas were made tall enough to easily click, even for the largest fingers.</p>
+            <h4>Tools</h4>
+            <p>Wireframes: Sketch</p><br>
+            <p>2D mockups: Illustrator, Sketch</p><br>
+            <p>Interactive mockups: InVision</p>
+        </li>
+    </ul>
+</div>
